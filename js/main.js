743047bd75389da2f6770cf7528df09084ca536c
Samuele-Benato <samuele.benato@gmail.com>
2023-09-05 11:00:26 +0200

    fix : main chat style corrected

--- js/main.js
+++ js/main.js
@@ -6,17 +6,17 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "10/01/2020 15:30:55",
+        date: " 15:30",
         message: "Hai portato a spasso il cane?",
         status: "sent",
       },
       {
-        date: "10/01/2020 15:50:00",
+        date: " 15:50",
         message: "Ricordati di stendere i panni",
         status: "sent",
       },
       {
-        date: "10/01/2020 16:15:22",
+        date: " 16:15",
         message: "Tutto fatto!",
         status: "received",
       },
@@ -28,17 +28,17 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "20/03/2020 16:30:00",
+        date: " 16:30",
         message: "Ciao come stai?",
         status: "sent",
       },
       {
-        date: "20/03/2020 16:30:55",
+        date: " 16:30",
         message: "Bene grazie! Stasera ci vediamo?",
         status: "received",
       },
       {
-        date: "20/03/2020 16:35:00",
+        date: " 16:35",
         message: "Mi piacerebbe ma devo andare a fare la spesa.",
         status: "sent",
       },
@@ -50,17 +50,17 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "28/03/2020 10:10:40",
+        date: "10:10",
         message: "La Marianna va in campagna",
         status: "received",
       },
       {
-        date: "28/03/2020 10:20:10",
+        date: "10:20",
         message: "Sicuro di non aver sbagliato chat?",
         status: "sent",
       },
       {
-        date: "28/03/2020 16:15:22",
+        date: " 16:15",
         message: "Ah scusa!",
         status: "received",
       },
@@ -72,12 +72,12 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "10/01/2020 15:30:55",
+        date: " 15:30",
         message: "Lo sai che ha aperto una nuova pizzeria?",
         status: "sent",
       },
       {
-        date: "10/01/2020 15:50:00",
+        date: " 15:50",
         message: "Si, ma preferirei andare al cinema",
         status: "received",
       },
@@ -89,12 +89,12 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "10/01/2020 15:30:55",
+        date: " 15:30",
         message: "Ricordati di chiamare la nonna",
         status: "sent",
       },
       {
-        date: "10/01/2020 15:50:00",
+        date: " 15:50",
         message: "Va bene, stasera la sento",
         status: "received",
       },
@@ -106,17 +106,17 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "10/01/2020 15:30:55",
+        date: " 15:30",
         message: "Ciao Claudia, hai novità?",
         status: "sent",
       },
       {
-        date: "10/01/2020 15:50:00",
+        date: " 15:50",
         message: "Non ancora",
         status: "received",
       },
       {
-        date: "10/01/2020 15:51:00",
+        date: "15:51",
         message: "Nessuna nuova, buona nuova",
         status: "sent",
       },
@@ -128,12 +128,12 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "10/01/2020 15:30:55",
+        date: " 15:30",
         message: "Fai gli auguri a Martina che è il suo compleanno!",
         status: "sent",
       },
       {
-        date: "10/01/2020 15:50:00",
+        date: " 15:50",
         message: "Grazie per avermelo ricordato, le scrivo subito!",
         status: "received",
       },
@@ -145,17 +145,17 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: "10/01/2020 15:30:55",
+        date: " 15:30",
         message: "Ciao, andiamo a mangiare la pizza stasera?",
         status: "received",
       },
       {
-        date: "10/01/2020 15:50:00",
+        date: " 15:50",
         message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
         status: "sent",
       },
       {
-        date: "10/01/2020 15:51:00",
+        date: " 15:51",
         message: "OK!!",
         status: "received",
       },
@@ -163,9 +163,9 @@ const contacts = [
   },
 ];
 
-let date = new Date();
-let minute = date.getMinutes();
-let hour = date.getHours();
+const date = new Date();
+const minute = date.getMinutes();
+const hour = date.getHours();
 
 const app = createApp({
   data() {
@@ -202,6 +202,10 @@ const app = createApp({
         status: "received",
       };
 
+      this.contacts[this.activeChat].messages.push(newMessage);
+    },
+
+    lastMessage() {
       this.contacts[this.activeChat].messages.push(newMessage);
     },
   },
